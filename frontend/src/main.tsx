f66cd8f732af742afc69f3f31855b5fb1eef2458
iyongsu <whgdkgo614@gmail.com>
2025-11-12 19:11:27 +0900

    init: my project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {BrowserRouter} from "react-router-dom";
+import React from "react";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+  </React.StrictMode>
 )
+
+/**
+ * 오류 #1 발생한이유 jsx -> ReactNode 로 변
+ * react-jsx 자동 jsx 변환이 설정이 되어있어도
+ * React.StrictMode같이 React 네임스페이스가 필요한 경우에는 import React 설정이 없을경우 깨짐.
+ */
